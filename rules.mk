2f2a24c8de51aa6aa9084449ec030c0f6d5fa3b6
Ergenekon Yiğit <yigitergenekon@gmail.com>
2021-02-27 02:19:17 +0300

    layers reorganized

--- rules.mk
+++ rules.mk
@@ -28,7 +28,8 @@ RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-AUDIO_ENABLE = no          # Audio output on port C6
+AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+UCIS_ENABLE = no            # Enable UCIS Unicode implementation
 
 LAYOUTS = planck_grid
